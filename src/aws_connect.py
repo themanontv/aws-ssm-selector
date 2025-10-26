fbd9b2e78ef493f9299ef9f5b19c3bfd826b528d
Joe Gadd <joe@computer.local>
2025-10-26 21:40:48 +0000

    reverted some changes

--- src/aws_connect.py
+++ src/aws_connect.py
@@ -31,10 +31,12 @@ def is_executable(file_path):
     Returns
     bool: True when shutil.which(file_path) locates a file and os.access(..., os.X_OK) is true; otherwise False.
     """
+
     executable_path = shutil.which(file_path)
     return bool(executable_path and os.access(executable_path, os.X_OK))
 
-def preview(selection, instance_data, instance_regex):
+
+def preview(selection):
     """
     Build and return a multi-line preview string for a selected instance entry.
 
@@ -68,121 +70,119 @@ def preview(selection, instance_data, instance_regex):
                     preview.append("IP: NONE")
                 return '\n'.join(preview)
 
-def main():
-    """
-    Create a selectable list of EC2 instances in the users aws account
-    """
+file_path = '/Users/<user>/.aws/config'
 
-    file_path = '/Users/<user>/.aws/config'
+# Try to fetch a profile from the environment
+os_profile = os.getenv('AWS_PROFILE')
 
-    # Regex Variables
-    instance_regex = re.compile(r'\(([\-a-z0-9]+)\)')
-    profile_regex = re.compile(r'\[profile ([a-zA-Z0-9\-]+)\]')
+# Regex Variables
+instance_regex = re.compile(r'\(([\-a-z0-9]+)\)')
+profile_regex = re.compile(r'\[profile ([a-zA-Z0-9\-]+)\]')
 
-    # Special environments list
-    hidden_environments = ['']
-    scary_environments = ['production']
+# Special environments list
+hidden_environments = ['']
+scary_environments = ['production']
 
-    if not os.getenv('AWS_PROFILE') and is_executable("aws"):
-        try:
-            with open(file_path, encoding='utf-8') as file:
-                text = file.read()
-            # Set regex to look for things with profile in it
-
-            profiles = []
-
-            # Split based on new lines and get the first group of the match
-            for line in text.splitlines():
-                match = re.search(profile_regex, line)
-                if match:
-                    if match.group(1) not in hidden_environments:
-                        profiles.append(match.group(1))
-        except:
-            print("No profiles found ")
-            sys.exit()
-        # Show the terminal menu for the profile list
-        terminal_menu = TerminalMenu(
-            menu_entries=profiles,
-            title="Select the desired profile ",
-            show_search_hint=True,
-            menu_highlight_style=("bg_green", "fg_black", "bold")
-        )
-        try:
-            profile = profiles[terminal_menu.show()]
-        except TypeError:
-            # If type is incorrect likely caused by exiting the menu
-            print("Ok, Bye. ")
-            sys.exit()
-    else:
-        profile = os.getenv('AWS_PROFILE')
-
-    # Set the colour of the highlight and present option to enter
-    if profile in scary_environments:
-        try:
-            if click.confirm(profile + ' looks scary, do you want to continue?', default=False, abort=True):
-                highlight = "bg_red"
-        except click.exceptions.Abort:
-            print('I understand. ')
-            sys.exit()
-    else:
-        highlight = "bg_green"
-
-    # Execute the describe instances command
-    if is_executable("aws-vault"):
-        result = subprocess.run(['aws-vault', 'exec', profile, '--', 'aws', 'ec2', 'describe-instances'], stdout=subprocess.PIPE, check=True)
-    else:
-        result = subprocess.run(['aws', 'ec2', 'describe-instances', '--profile', profile], stdout=subprocess.PIPE, check=True)
-
-    # Parse the JSON
+if not os_profile and is_executable("aws"):
+    # Reading from the ~/.aws/config file to get the profiles
     try:
-        parsed = json.loads(result.stdout)
-    except json.decoder.JSONDecodeError:
-        print("I don't think you're allowed to do that ")
+        with open(file_path, encoding='utf-8') as file:
+            text = file.read()
+        # Set regex to look for things with profile in it
+
+        profiles = []
+
+        # Split based on new lines and get the first group of the match
+        for line in text.splitlines():
+            match = re.search(profile_regex, line)
+            if match:
+                if match.group(1) not in hidden_environments:
+                    profiles.append(match.group(1))
+    except:
+        print("No profiles found ")
         sys.exit()
-    instance_data = parsed['Reservations']
-
-    items = []
-
-    # Get the names and instance IDs from the JSON
-    for reservation in instance_data:
-        for instance in reservation['Instances']:
-            name = ""
-            # Get the Name tag value and make it the name
-            try:
-                for tag in instance["Tags"]:
-                    if tag["Key"] == "Name":
-                        name = tag["Value"]
-                    else:
-                        pass
-            except:
-                name = ""
-            if name == "":
-                name = "NONAME"
-            items.append(f'{name:<30s} ({instance["InstanceId"]:<15s})')
-
-    # Show the terminal menu for the instance list
+    # Show the terminal menu for the profile list
     terminal_menu = TerminalMenu(
-        menu_entries=items,
-        title="Select an instance in " + profile + " ",
+        menu_entries=profiles,
+        title="Select the desired profile ",
         show_search_hint=True,
-        preview_command=preview,
-        preview_title="Details",
-        menu_highlight_style=(highlight, "fg_black", "bold")
+        menu_highlight_style=("bg_green", "fg_black", "bold")
     )
-    index = terminal_menu.show()
-
     try:
-        match = re.search(instance_regex, items[index])
+        profile = profiles[terminal_menu.show()]
     except TypeError:
         # If type is incorrect likely caused by exiting the menu
-        print("No Instance selected ")
+        print("Ok, Bye. ")
         sys.exit()
+else:
+    profile = os_profile
 
-    # Execute the describe instances command
-    if is_executable("aws-vault"):
-        os.system('aws-vault exec ' + profile + ' -- aws ssm start-session --target ' + match.group(1))
-    else:
-        os.system('aws ssm start-session --target ' + match.group(1) + ' --profile ' + profile)
+# Set the colour of the highlight and present option to enter
+if profile in scary_environments:
+    try:
+        if click.confirm(profile + ' looks scary, do you want to continue?', default=False, abort=True):
+            highlight = "bg_red"
+    except click.exceptions.Abort:
+        print('I understand. ')
+        sys.exit()
+else:
+    highlight = "bg_green"
 
-if __name__ == '__main__':
-    main()
+# Execute the describe instances command
+if is_executable("aws-vault"):
+    result = subprocess.run(['aws-vault', 'exec', profile, '--', 'aws', 'ec2', 'describe-instances'], stdout=subprocess.PIPE, check=True)
+else:
+    result = subprocess.run(['aws', 'ec2', 'describe-instances', '--profile', profile], stdout=subprocess.PIPE, check=True)
+
+# Parse the JSON
+try:
+    parsed = json.loads(result.stdout)
+except json.decoder.JSONDecodeError:
+    print("I don't think you're allowed to do that ")
+    sys.exit()
+instance_data = parsed['Reservations']
+
+items = []
+
+# Get the names and instance IDs from the JSON
+for reservation in instance_data:
+    for instance in reservation['Instances']:
+        name = ""
+        # Get the Name tag value and make it the name
+        try:
+            for tag in instance["Tags"]:
+                if tag["Key"] == "Name":
+                    name = tag["Value"]
+                else:
+                    pass
+        except:
+            name = ""
+        if name == "":
+            name = "NONAME"
+        id = instance["InstanceId"]
+        items.append(f'{name:<30s} ({id:<15s})')
+
+# Show the terminal menu for the instance list
+terminal_menu = TerminalMenu(
+    menu_entries=items,
+    title="Select an instance in " + profile + " ",
+    show_search_hint=True,
+    preview_command=preview,
+    preview_title="Details",
+    menu_highlight_style=(highlight, "fg_black", "bold")
+)
+index_2 = terminal_menu.show()
+
+try:
+    match = re.search(instance_regex, items[index_2])
+except TypeError:
+    # If type is incorrect likely caused by exiting the menu
+    print("No Instance selected ")
+    sys.exit()
+id = match.group(1)
+
+# Execute the describe instances command
+if is_executable("aws-vault"):
+    os.system('aws-vault exec ' + profile + ' -- aws ssm start-session --target ' + id)
+else:
+    os.system('aws ssm start-session --target ' + id + ' --profile ' + profile)
