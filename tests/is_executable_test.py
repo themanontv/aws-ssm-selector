d286ec50814df83abb45a3e4f0c6c24afe217705
Joe Gadd <joe@computer.local>
2025-10-27 10:14:31 +0000

    fixed failing issues with unit tests and moved to main

--- tests/is_executable_test.py
+++ tests/is_executable_test.py
@@ -7,16 +7,15 @@ from unittest import mock
 import shutil
 from aws_connect import is_executable
 
-class TestFilterMetrics(unittest.TestCase):
+class TestExecutable(unittest.TestCase):
     """
     Test cases for the is_executable function
     """
     @mock.patch("aws_connect.os.access", return_value=True)
     @mock.patch("aws_connect.shutil.which", return_value="/usr/local/bin/aws")
     def test_is_executable_returns_true(self, mock_which, mock_access):
-        # Act
+        
         result = is_executable("aws")
-        # Assert
         self.assertTrue(result)
         mock_which.assert_called_once_with("aws")
         mock_access.assert_called_once_with("/usr/local/bin/aws", os.X_OK)
